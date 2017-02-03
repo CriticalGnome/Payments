934159861db5a8b5e93c824de98242821da91c13
Сергей Калиновский <lord.skiminok@gmail.com>
2017-02-03 23:16:22 +0300

    make shine

--- src/com/criticalgnome/payments/actions/register.java
+++ src/com/criticalgnome/payments/actions/register.java
@@ -37,57 +37,55 @@ public class register extends HttpServlet {
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8"); 
 		Boolean isError = false;
-		String stringError = "register.jsp?";
+		String redirectUrl = "register.jsp?";
 
 		if (request.getParameter("firstName").equals("")) {
 			isError = true;
-			stringError = stringError + "fnError=empty&";
+			redirectUrl = redirectUrl + "fnError=empty&";
 		} else {
 			if (!checkWithRegExp(request.getParameter("firstName"))) {
 				isError = true;
-				stringError = stringError + "fnError=wrong&";
+				redirectUrl = redirectUrl + "fnError=wrong&";
 			} 
-			stringError = stringError + "firstName=" + URLEncoder.encode(request.getParameter("firstName")) + "&";
+			redirectUrl = redirectUrl + "firstName=" + URLEncoder.encode(request.getParameter("firstName")) + "&";
 		}
 		if (request.getParameter("lastName").equals("")) {
 			isError = true;
-			stringError = stringError + "lnError=empty&";
+			redirectUrl = redirectUrl + "lnError=empty&";
 		} else {
 			if (!checkWithRegExp(request.getParameter("lastName"))) {
 				isError = true;
-				stringError = stringError + "lnError=wrong&";
+				redirectUrl = redirectUrl + "lnError=wrong&";
 			} 
-			stringError = stringError + "lastName=" + URLEncoder.encode(request.getParameter("lastName")) + "&";
+			redirectUrl = redirectUrl + "lastName=" + URLEncoder.encode(request.getParameter("lastName")) + "&";
 		}
 		if (request.getParameter("email").equals("")) {
 			isError = true;
-			stringError = stringError + "emError=empty&";
+			redirectUrl = redirectUrl + "emError=empty&";
 		} else {
-			stringError = stringError + "email=" + URLEncoder.encode(request.getParameter("email")) + "&";
+			redirectUrl = redirectUrl + "email=" + URLEncoder.encode(request.getParameter("email")) + "&";
 		}
 		if (request.getParameter("password").equals("")) {
 			isError = true;
-			stringError = stringError + "pwError=empty&";
+			redirectUrl = redirectUrl + "pwError=empty&";
 		} else {
 			if (!checkWithRegExp(request.getParameter("password"))) {
 				isError = true;
-				stringError = stringError + "pwError=wrong&";
+				redirectUrl = redirectUrl + "pwError=wrong&";
 			} 
-			stringError = stringError + "password=" + URLEncoder.encode(request.getParameter("password")) + "&";
+			redirectUrl = redirectUrl + "password=" + URLEncoder.encode(request.getParameter("password")) + "&";
 		}
-		if (isError) {
-			response.sendRedirect(stringError);
+		if (!isError) {
+			redirectUrl = "index.jsp?action=newuser";
+			try {
+				UserDAO.getInstance().addUser(request.getParameter("firstName"), request.getParameter("lastName"), request.getParameter("email"), request.getParameter("password"));
+			} catch (MySQLIntegrityConstraintViolationException e) {
+				redirectUrl = "register.jsp?action=emailalreadyexist";
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
-		// No Errors, adding user to DB
-		String defaultRedirect = "index.jsp?action=newuser";
-		try {
-			UserDAO.getInstance().addUser(request.getParameter("firstName"), request.getParameter("lastName"), request.getParameter("email"), request.getParameter("password"));
-		} catch (MySQLIntegrityConstraintViolationException e) {
-			defaultRedirect = "register.jsp?action=emailalreadyexist";
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		response.sendRedirect(defaultRedirect);
+		response.sendRedirect(redirectUrl);
 	}
 	public static boolean checkWithRegExp(String userNameString){  
         Pattern p = Pattern.compile("^[а-яА-ЯёЁa-zA-Z0-9]+$");  
